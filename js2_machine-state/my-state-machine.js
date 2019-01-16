6f95d260e939d55a7f07b912fa755677c570d3e5
SranXXL <37510256+SranXXL@users.noreply.github.com>
2019-01-16 16:11:33 +0300

    Fixed multiple machines
    Fixed inability to create multiple machines

--- js2_machine-state/my-state-machine.js
+++ js2_machine-state/my-state-machine.js
@@ -1,114 +1,86 @@
-let curMachine;
-let curEvent;
+const machineCallStack = [];
 
 function useContext() {
+	const curMachine = machineCallStack[machineCallStack.length - 1][0];
 	return [curMachine.context, (newContext) => {Object.assign(curMachine.context, newContext);}];
 }
 
 function useState() {
+	const curMachine = machineCallStack[machineCallStack.length - 1][0];
+	const curEvent = machineCallStack[machineCallStack.length - 1][1];
 	return [curMachine.state, (newState) => {
-		if (curMachine.states[newState] === undefined) {
+		if (curMachine.states[newState] === undefined)
 			throw new MachineException('Error! Unknown state!');
-		}
 		try {
-			doStateAction(curMachine.states[curMachine.state].onExit);
+			doStateAction(curMachine, curEvent, curMachine.states[curMachine.state].onExit);
 			curMachine.state = newState;
-			doStateAction(curMachine.states[curMachine.state].onEntry);
+			doStateAction(curMachine, curEvent, curMachine.states[newState].onEntry);
 		} catch (e) {
 			console.log(e.message);
 		}
 	}];
 }
 
-function doStateAction(stateAction) {
+function fillStack(machine, event, func) {
+	machineCallStack.push([machine, event]);
+	func(event);
+	machineCallStack.pop();
+}
+
+function doStateAction(curMachine, event, stateAction) {
 	if (typeof(stateAction) === "function") {
-		stateAction(event);
+		fillStack(curMachine, event, stateAction);
 	} else if (typeof(stateAction) === "string") {
 		if (curMachine.actions[stateAction] === undefined) {
 			throw new MachineException('Error! Unknown action!');
 		}
-		curMachine.actions[stateAction](event);
+		fillStack(curMachine, event, curMachine.actions[stateAction]);
 	} else if (typeof(stateAction) === typeof([0])) {
 		for (let action in stateAction) {
 			let act = stateAction[action];
 			if (curMachine.actions[act] === undefined) {
 				throw new MachineException('Error! Unknown action!');
 			}
-			curMachine.actions[act](event);
+			fillStack(curMachine, event, curMachine.actions[act]);
 		}
 	} else {
 		throw new MachineException('Error! Unknown type of state action!');
 	}
 }
 
-function MachineException(message) {
-		this.message = message;
-}
-
 function machine(newMachine) {
 	const stateMachine = {
 		id: newMachine.id,
-    state: newMachine.initialState,
-    context: newMachine.context,
-    states: newMachine.states,
-    actions: newMachine.actions,
-    transition(transaction, event) {
-    	if (this.states[this.state].on === undefined) {
-    		throw new MachineException('Error! Current state doesn`t have field "on"!');
-    	}
-    	const trans = this.states[this.state].on[transaction];
-    	if (trans === undefined) {
-    		throw new MachineException('Error! Unknown transaction!');
-    	}
-    	curMachine = this;
-    	curEvent = event;
-    	if (trans.service !== undefined) {
-    		trans.service(event);
-    	} else if (trans.target !== undefined) {
-    		const [state, setState] = useState();
-    		setState(trans.target);
-    	} else {
-    		throw new MachineException('Error! Transaction must have field "service" or "target"!');
-    	}
-    	return 0;
-    }
+		state: newMachine.initialState,
+		context: newMachine.context,
+		states: newMachine.states,
+		actions: newMachine.actions,
+		transition(transaction, event) {
+			if (this.states[this.state].on === undefined) 
+				throw new MachineException('Error! Current state doesn`t have field "on"!');
+			const trans = this.states[this.state].on[transaction];
+			if (trans === undefined) throw new MachineException('Error! Unknown transaction!');
+			if (trans.service !== undefined) {
+				fillStack(this, event, trans.service);
+			} else if (trans.target !== undefined) {
+				machineCallStack.push([this, event]);
+				const [state, setState] = useState();
+				setState(trans.target);
+				machineCallStack.pop();
+			} else throw new MachineException('Error! Transaction must have field "service" or "target"!');
+			return 0;
+		}
 	};
 	return stateMachine;
 }
 
-// TESTS
-// № 1. No field 'on'.
-const vacancyMachine0 = machine({
-	id: 'vacancy',
-	initialState: 'responded',
-	context: {id: 0},
-	states: {
-		responded: {
-			onEntry: 'onStateEntry'
-		},
-		notResponded: {                       
-			onExit() {
-				console.log('we are leaving notResponded state');
-			}
-		}
-	}, 
-	actions: {
-    onStateEntry: (event) => {
-      const [state] = useState();
-		  console.log('now state is ' + state);
-	  }
-	}
-})
-
-try {
-	vacancyMachine0.transition('BAK', {resume: {name: 'Vasya', lastName: 'Pupkin'}});	
-} catch (e) {
-			console.log('Test 1:');
-			console.log(e.message);
+function MachineException(message) {
+	this.message = message;
 }
 
-// № 2. Unknown transaction.
-const vacancyMachine1 = machine({
+// **************************	TESTS	************************
+// № 1. Machine from StateMachinePseudoApi.
+const vacancyMachine = machine({
 	id: 'vacancy',
 	initialState: 'notResponded',
 	context: {id: 1},
@@ -116,75 +88,110 @@ const vacancyMachine1 = machine({
 		responded: {
 			onEntry: 'onStateEntry'
 		},
-		notResponded: {                       
+		notResponded: {                        
+			onExit() {
+				console.log('1 we are leaving notResponded state');
+			},
 			on: {
-        // Транзакция
-				RESPOND: {          
-				  target: 'responded'
+				RESPOND: {
+					service: (event) => {
+						const [context, setContext] = useContext();
+						const [state, setState] = useState();
+						setTimeout(() => {
+							setState('responded');
+							setContext({completed: true});
+						}, 1000);
+					},
+					target: 'responded'
 				}
 			}
-		}
-	}, 
+		}		
+	},
 	actions: {
-    onStateEntry: (event) => {
-      const [state] = useState();
-		  console.log('now state is ' + state);
-	  }
+		onStateEntry: (event) => {
+			const [state] = useState();
+			console.log('1 now state is ' + state);
+		},
+		makeResponse: () => {
+			setTimeout(() => {
+				console.log("Response.");
+			}, 1000);
+		}
 	}
 })
 
-try {
-	vacancyMachine1.transition('SMTH', {resume: {name: 'Vasya', lastName: 'Pupkin'}});	
-} catch (e) {
-			console.log('Test 2:');
-			console.log(e.message);
-}
-
-// № 3. Without service.
-console.log('Test 3:');
-vacancyMachine1.transition('RESPOND', {resume: {name: 'Vasya', lastName: 'Pupkin'}});
-
-// № 4. State action is array.
-const vacancyMachine2 = machine({
+// № 2. State action is array.
+const vacancyMachine1 = machine({
 	id: 'vacancy',
 	initialState: 'notResponded',
 	context: {id: 2},
 	states: {
 		responded: {
-      // action, который нужно выполнить при входе в это состояние. Можно задавать массивом, строкой или функцией
 			onEntry: ['onStateEntry', 'makeResponse']
 		},
 		notResponded: {                        
 			onExit() {
-				console.log('we are leaving notResponded state');
+				console.log('2 we are leaving notResponded state');
 			},
 			on: {
 				RESPOND: {
-          service: (event) => {
-            const [context, setContext] = useContext();
-            const [state, setState] = useState();
-            setTimeout(() => {
-            	setState('responded');
-              setContext({completed: true});
-            }, 1000);
-          },
-          target: 'responded'
-        }
+					service: (event) => {
+						const [context, setContext] = useContext();
+						const [state, setState] = useState();
+						setTimeout(() => {
+							setState('responded');
+							setContext({completed: true});
+						}, 3000);
+					},
+					target: 'responded'
+				}
 			}
 		}		
 	},
 	actions: {
-    onStateEntry: (event) => {
-      const [state] = useState();
-		  console.log('now state is ' + state);
-	  },
-	  makeResponse: (event) => {
-      setTimeout(() => {
-      	console.log("Response.");
-      }, 1000);
-    }
+		onStateEntry: (event) => {
+			const [state] = useState();
+			console.log('2 now state is ' + state);
+		},
+		makeResponse: () => {
+			setTimeout(() => {
+				console.log("2 Response.");
+			}, 1000);
+		}
 	}
 })
 
-console.log('Test 4:');
+// № 3. Without service.
+const vacancyMachine2 = machine({
+	id: 'vacancy',
+	initialState: 'notResponded',
+	context: {id: 1},
+	states: {
+		responded: {
+			onEntry: 'onStateEntry'
+		},
+		notResponded: {                        
+			onExit() {
+				console.log('3 we are leaving notResponded state');
+			},
+			on: {
+				RESPOND: {
+					target: 'responded'
+				}
+			}
+		}		
+	},
+	actions: {
+		onStateEntry: (event) => {
+			const [state] = useState();
+			console.log('3 now state is ' + state);
+		}
+	}
+})
+
+console.log('Test 1. Machine from StateMachinePseudoApi:');
+vacancyMachine.transition('RESPOND', {resume: {name: 'Vasya', lastName: 'Pupkin'}});
+console.log('Test 2. State action is array:');
+vacancyMachine1.transition('RESPOND', {resume: {name: 'Vasya', lastName: 'Pupkin'}});
+console.log('Test 3. Without service:');
 vacancyMachine2.transition('RESPOND', {resume: {name: 'Vasya', lastName: 'Pupkin'}});
